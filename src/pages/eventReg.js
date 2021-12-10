44b43288869e37f96a0d9667cfc05fba97463414
deeppatel23 <deeppatel2.2312@gmail.com>
2021-12-10 10:01:07 +0530

    register and feedback route dynamic

--- src/pages/eventReg.js
+++ src/pages/eventReg.js
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const EventReg = () => {
-  return (
-    <html>
-      <head>
-        <meta http-equiv="refresh" content="0; url='https://forms.gle/7zXFonn18MV6oSDz7'" />
-      </head>
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    fetch("https://csiddu-website-backend.herokuapp.com/events")
+      .then(res => res.json())
+      .then(
+        (data) => {
+          setIsLoaded(true);
+          setEvents(data);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
 
-      <div className='p-10 flex flex-col font-franklin'>
-      
-      <div className="lg:px-10 self-center mt-36  h-screen">
-        <p className="text-7xl font-extrabold">Just a moment</p>
+  const Loading = () => {
+    return (
+      <div class="flex w-screen h-screen justify-center items-center">
+        <div class="animate-spin rounded-full h-28 w-28 border-b-2 border-gray-600"></div>
       </div>
+    )
+  }
 
-    </div>
-    </html>
-  );
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <Loading />
+  } else {
+    return (
+      <div>
+        {events.sort((a, b) => (a.id) > (b.id) ? -1 : 0).map(event => (
+          event.register ?
+            <html>
+              <head>
+                <meta http-equiv="refresh" content={"0; url=" + event.rlink} />
+
+              </head>
+
+              <div className='p-10 flex flex-col font-franklin'>
+
+                <div className="lg:px-10 self-center mt-36  h-screen">
+                  <p className="text-7xl font-extrabold">Just a moment</p>
+                </div>
+
+              </div>
+            </html> :
+            null
+        ))}
+      </div>
+    );
+  }
 };
 
 export default EventReg;
