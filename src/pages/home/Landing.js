784d36b9048bc018e3695c624176a2d63bb9bbf0
deeppatel23 <deeppatel2.2312@gmail.com>
2022-01-20 17:18:01 +0530

    made heading responsive

--- src/pages/home/Landing.js
+++ src/pages/home/Landing.js
@@ -9,7 +9,7 @@ function Landing() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
           <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-8xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Computer Society Of India <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-700 to-blue-400">Student Branch DDU</span></h1>
+            <h1 className="text-5xl md:text-8xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Computer Society Of India <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-700 to-blue-400"> <br />Student Branch DDU</span></h1>
             <div className="max-w-3xl mx-auto">
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">We envision a dream where everyone is provided an opportunity to expand their horizons of knowledge. </p>
               <div className="p-10 max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
